d0fbf8416ad595f472cfb99addba82912cc9b791
Oskar Wiszowaty <ppwiszu@gmail.com>
2023-10-28 23:54:35 +0200

    Example 2

---
+++ example2-2/example2-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace example2_2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ServiceCollection services = new ServiceCollection();
+
+            services.AddScoped<A>();
+            services.AddScoped<B>();
+            services.AddScoped<C>();
+            services.AddScoped<D>();
+            services.AddScoped<E>();
+            
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+            
+            E e = serviceProvider.GetService<E>();
+        }
+    }
+}
