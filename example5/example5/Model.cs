9e07a479f6488c64e4e4e5a5bcc99d8a5d0b51a4
Oskar Wiszowaty <ppwiszu@gmail.com>
2023-10-29 00:15:28 +0200

    Example 4

---
+++ example5/example5/Model.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace example5
+{
+    public interface MyInterface
+    {
+        void DoSomething();
+    }
+    
+    public class A : MyInterface
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("A done something");
+        }
+    }
+    
+    public class B : MyInterface
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("B done something");
+        }
+    }
+}
---
+++ example5/example5/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace example5
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ServiceCollection services = new ServiceCollection();
+
+            services.AddSingleton<MyInterface, A>();
+            services.AddSingleton<MyInterface, B>();
+            
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+            
+            var coto = serviceProvider.GetService<MyInterface>();
+            
+            coto.DoSomething();
+        }
+    }
+}
